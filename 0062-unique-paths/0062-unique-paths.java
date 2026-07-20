e180c381ff376ec884841f74261ef98718075a13
Samhita <159715238+samhi7@users.noreply.github.com>
2026-07-20 22:55:29 +0530

    Time: 1 ms (14.61%), Space: 42 MB (82.81%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m][n];
+        int up=0;
+        int down=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 & j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                up=0;
+                down=0;
+                if(i>0){
+                    up=dp[i-1][j];
+                }
+                if(j>0){
+                    down=dp[i][j-1];
+                }
+                dp[i][j]=up+down;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
